37bcbcce47f4eba66fb720fd2aab56f08647323c
helloiamnoob <86592051+helliamnoob@users.noreply.github.com>
2023-07-05 20:17:49 +0900

    Apply .gitignore

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const userRouter = require("./routes/users.route")
 const app = express();
-const port = 3000;
-
-const indexRouter = require("./routes/index.js");
-const commentsRouter = require("./routes/comments.js");
-const postsRouter = require("./routes/posts.js");
-const connect = require("./schemas");
-
-connect();
+const PORT = 3000;
 
 app.use(express.json());
+app.use(cookieParser());
+app.use('/api', [userRouter]);
 
-app.use("/", [indexRouter, commentsRouter, postsRouter]);
-
-app.listen(port, () => {
-  console.log(port, "포트로 서버가 열렸");
-});
+app.listen(PORT, () => {
+  console.log(PORT, '포트 번호로 서버가 실행되었습니다.');
+})
